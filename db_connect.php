ae99d7a54f5c85485a2484dffdaf5b6ddb24aeec
simonkamate <simonkamat@gmail.com>
2025-04-11 22:38:10 +0000

    first commit

---
+++ db_connect.php
@@ -0,0 +1,15 @@
+<?php
+$host = 'localhost';
+$dbname = 'mglsi_news';
+$username = 'mglsi_user';
+$password = 'passer';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->exec("SET NAMES 'utf8'");
+} catch (PDOException $e) {
+    // Envoyer l'erreur à Sentry
+    \Sentry\captureException($e);
+    die("Échec de la connexion : " . $e->getMessage());
+}
